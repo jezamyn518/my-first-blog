7631dab38c1bd3798696d0f5ff4d57177bbfa2cb
jezamyn <jeza.oyao28@gmail.com>
2022-02-17 22:12:35 +0800

    update

--- blog/models.py
+++ blog/models.py
@@ -17,8 +17,6 @@ class Post(models.Model):
     def __str__(self):
         return self.title
     
-    def approved_comments(self):
-       return self.comments.filter(approved_comment=True)
 
 class Comment(models.Model):
     post = models.ForeignKey('blog.Post', on_delete=models.CASCADE, related_name='comments')
@@ -33,3 +31,6 @@ class Comment(models.Model):
 
     def __str__(self):
         return self.text
+
+    def approved_comments(self):
+       return self.comments.filter(approved_comment=True)
--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,17 @@
-from django.shortcuts import redirect
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from django.utils import timezone
-from .models import Post, Comment
-from .form import PostForm
+from .models import Post
+from django.shortcuts import render, get_object_or_404
+from .forms import PostForm
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
-from .form import PostForm, CommentForm
-
+from .forms import PostForm, CommentForm
+from .models import Post, Comment
+# Create your views here.
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
-
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
@@ -24,7 +24,6 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
@@ -39,7 +38,6 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
@@ -60,22 +58,9 @@ def post_publish(request, pk):
 @login_required
 def post_remove(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    post.remove()
-    return redirect('post_detail', pk=pk)
-    
+    post.delete()
+    return redirect('post_list')
 
-
-@login_required
-def comment_approve(request, pk):
-    comment = get_object_or_404(Comment, pk=pk)
-    comment.approve()
-    return redirect('post_detail', pk=comment.post.pk)
-
-@login_required
-def comment_remove(request, pk):
-    comment = get_object_or_404(Comment, pk=pk)
-    comment.delete()
-    return redirect('post_detail', pk=comment.post.pk)
 
 def add_comment_to_post(request, pk):
     post = get_object_or_404(Post, pk=pk)
@@ -89,4 +74,16 @@ def add_comment_to_post(request, pk):
     else:
         form = CommentForm()
     return render(request, 'blog/add_comment_to_post.html', {'form': form})
+
+@login_required
+def comment_approve(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('post_detail', pk=comment.post.pk)
+
+@login_required
+def comment_remove(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.delete()
+    return redirect('post_detail', pk=comment.post.pk)
 
